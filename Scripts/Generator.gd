9330c7d68b8765075dbf9011b859620d2df76ae1
Devendra <devendramokal806@gmail.com>
2024-08-04 03:07:47 +0530

    Added Graphic, Added Player and Remove all Unnecessary threadings (Which are all)

--- Scripts/Generator.gd
+++ Scripts/Generator.gd
@@ -1,47 +1,43 @@
 extends Node2D
-var thread : Thread
-var thread1 : Thread
-var thread2 : Thread
 var biome : BiomeGen
-var height := 200
-var width := 200
-#var tile := {
-	#"tileName":{
-		#"layer": 0, 
-		#"source_id": -1, 
-		#"atlas_coords": Vector2i(-1, -1)
-	#}
-#}
+var height := 40
+var width := 61
+var world_seed : int
+var player_tile_pos
+@onready var tile_map = $TileMap
+@export var player: CharacterBody2D 
+
+var tiles := {
+	"sea":[0, 0, [Vector2i(27, 5)]],
+	"sand": [0, 0, [Vector2i(19, 9)]],
+	"land":[0, 0, [Vector2i(1, 9),Vector2i(0, 11), Vector2i(1, 11), Vector2i(2, 11), Vector2i(4, 11)]]
+}
 var biome_info := {
-	"sea":{
-		"alt": [-1, 0.01],
-		"ppt": [-1, 1],
-		"tem": [-1, 1]
-	},
-	"land":{
-		"alt": [0.01, 1],
-		"ppt": [-1, 1],
-		"tem": [-1, 1]
-	}
+	"sea": [[-2, 0.04], [-2, 2], [-2, 2]],
+	"sand": [[0.04, 0.1], [-2, 2], [-2, 2]],
+	"land": [[0.1, 2], [-2, 2], [-2, 2]]
 }
 
-func threading()-> void:
-	thread1 = Thread.new()
-	thread2 = Thread.new()
-	thread1.start(func(): biome = BiomeGen.new(self.height, self.width))
-	thread1.wait_to_finish()
-	thread2.start(biome.set_biomes.bind(biome_info))
+func Generating()-> void:
+	world_seed = randi()
+	biome = BiomeGen.new(self.height*4, self.width*4, world_seed)
+	biome.set_biomes(biome_info, tile_map.local_to_map(player.position))
+	biome.render_tiles(tiles, tile_map, tile_map.local_to_map(player.position))
+	biome.height = self.height
+	biome.width = self.width
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	thread = Thread.new()
-	thread.start(threading)
+	Generating()
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func threading():
 	pass
 
-func _exit_tree():
-	thread.wait_to_finish()
-	thread1.wait_to_finish()
-	thread2.wait_to_finish()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player_tile_pos != tile_map.local_to_map(player.position):
+		player_tile_pos = tile_map.local_to_map(player.position)
+		biome.set_biomes(biome_info, player_tile_pos)
+		biome.render_tiles(tiles, tile_map, player_tile_pos)
+		biome.unload_distant_chunks(player_tile_pos, tile_map)
+
--- Scripts/biomeGen.gd
+++ Scripts/biomeGen.gd
@@ -2,42 +2,21 @@ class_name BiomeGen
 
 var height : int
 var width : int
-var alt := {}
-var ppt := {}
-var tem := {}
 var biomes := {}
 var alt_noise := FastNoiseLite.new()
 var ppt_noise := FastNoiseLite.new()
 var tem_noise := FastNoiseLite.new()
-var thread_alt : Thread
-var thread_ppt : Thread
-var thread_tem : Thread
 
-func _init(height: int,width: int):
+func _init(height: int, width: int, world_seed: int):
 	self.height = height
 	self.width = width
-	self.pre_noiseSetup()
-	self.thread_alt = Thread.new()
-	self.thread_ppt = Thread.new()
-	self.thread_tem = Thread.new()
-	self.thread_alt.start(set_alt)
-	self.thread_ppt.start(set_ppt)
-	self.thread_tem.start(set_tem)
-	if (self.thread_alt.is_alive() or 
-		self.thread_ppt.is_alive() or 
-		self.thread_tem.is_alive()):
-		self.thread_alt.wait_to_finish()
-		self.thread_ppt.wait_to_finish()
-		self.thread_tem.wait_to_finish()
+	self.pre_noiseSetup(world_seed)
 
-func set_alt()-> void:
-	self.alt = self.value_from_noise(self.alt_noise)
-func set_ppt()-> void:
-	self.ppt = self.value_from_noise(self.ppt_noise)
-func set_tem()-> void:
-	self.tem = self.value_from_noise(self.tem_noise)
-
-func pre_noiseSetup()-> void:
+func pre_noiseSetup(world_seed: int)-> void:
+	self.alt_noise.seed = world_seed
+	self.ppt_noise.seed = world_seed
+	self.tem_noise.seed = world_seed
+	self.alt_noise.frequency = 0.01
 	self.ppt_noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
 	self.ppt_noise.frequency = 0.005
 	self.ppt_noise.fractal_octaves = 3
@@ -48,35 +27,51 @@ func pre_noiseSetup()-> void:
 	self.tem_noise.fractal_lacunarity = 3.23
 	self.tem_noise.fractal_gain = 0.17
 	self.tem_noise.fractal_weighted_strength = -0.52
-	
-func value_from_noise(noise: FastNoiseLite)-> Dictionary: 
-	var gridValue := {}
-	for x in self.width+1:
-		for y in self.height+1:
-			var value := noise.get_noise_2d(x,y)
-			gridValue[Vector2(x,y)] = value
-	return gridValue 
 
 func between(val, start, end)-> bool:
 	if start <= val and val < end:
 		return true
 	return false
 
-func set_biomes(biome_info: Dictionary, 
-				tilemap: TileMap=null, tiles: Dictionary={})-> void:
-	for x in self.width+1:
-		for y in self.height+1:
-			var pos := Vector2(x,y)
-			var altitude: float = self.alt[pos]
-			var precipition: float = self.ppt[pos]
-			var temprature: float = self.tem[pos]
-			for key in biome_info:
-				if (between(altitude, biome_info[key]["alt"][0], biome_info[key]["alt"][1]) and 
-					between(precipition, biome_info[key]['ppt'][0], biome_info[key]['ppt'][1]) and 
-					between(temprature, biome_info[key]['tem'][0], biome_info[key]['tem'][1])):
-					self.biomes[pos] = key
-					if not tiles.is_empty():
-						if tilemap != null:
-							tilemap.set_cell(tiles[key]["layer"], pos,
-										tiles[key]["source_id"], 
-										tiles[key]["atlas_coords"])
+func set_biomes(biome_info: Dictionary, player_pos: Vector2i = Vector2i(0,0))-> void:
+	for x in self.width:
+		for y in self.height:
+			var pos := Vector2i(player_pos.x - (width/2) + x, player_pos.y - (height/2) + y)
+			var altitude: float = self.alt_noise.get_noise_2d(pos.x, pos.y)
+			var precipition: float = self.ppt_noise.get_noise_2d(pos.x, pos.y)
+			var temprature: float = self.tem_noise.get_noise_2d(pos.x, pos.y)
+			for key in biome_info.keys():
+				if (between(altitude, biome_info[key][0][0], biome_info[key][0][1]) and 
+					between(precipition, biome_info[key][1][0], biome_info[key][1][1]) and 
+					between(temprature, biome_info[key][2][0], biome_info[key][2][1])):
+						if not self.biomes.has(pos):
+							self.biomes[pos] = [key, Vector2i(-1,-1)]
+
+func render_tiles(tiles: Dictionary, tilemap: TileMap, player_pos: Vector2i)-> void:
+	for x in self.width:
+		for y in self.height:
+			var pos = Vector2i(player_pos.x - (width/2) + x, player_pos.y - (height/2) + y)
+			var biome: String = biomes[Vector2i(pos)][0]
+			if biomes[Vector2i(pos)][1] == Vector2i(-1,-1):
+				var temp = tiles[biome][2].pick_random()
+				tilemap.set_cell(tiles[biome][0], Vector2i(player_pos.x - (width/2) + x, player_pos.y - (height/2) + y), tiles[biome][1], temp)
+				self.biomes[Vector2i(pos)][1] = temp
+			else:
+				tilemap.set_cell(tiles[biome][0], Vector2i(player_pos.x - (width/2) + x, player_pos.y - (height/2) + y), tiles[biome][1], biomes[Vector2i(pos)][1])
+				
+func unload_distant_chunks(player_pos, tilemap: TileMap)-> void:
+	var unload_distance_threshold := (width * 3) + 1
+	for chunk in biomes:
+		var distance_to_player = get_dist(chunk, player_pos)
+		if distance_to_player > unload_distance_threshold:
+			clear_chunk(chunk, tilemap)
+			biomes.erase(chunk)
+
+func get_dist(p1, p2)-> float:
+	var resultant = p1 - p2
+	return sqrt(resultant.x ** 2 + resultant.y ** 2)
+	
+func clear_chunk(pos, tilemap: TileMap)-> void:
+	for x in range(width):
+		for y in range(height):
+			tilemap.set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), -1, Vector2(-1, -1), -1)
