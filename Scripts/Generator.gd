94e4648ca2885e01e84c25d32a5def55311cd585
Devendra <devendramokal806@gmail.com>
2024-08-15 19:06:40 +0530

    Created a inner class named Biome for 2d specific generation, and created some getter functions in BiomeGen. And Cleaned the code.

--- Scripts/Generator.gd
+++ Scripts/Generator.gd
@@ -1,21 +1,21 @@
 extends Node2D
-var biome : BiomeGen
+var biome: BiomeGen
 var height := 40
 var width := 61
-var world_seed : int
+var world_seed: int
 var player_tile_pos
 @onready var tile_map = $TileMap
-@export var player: CharacterBody2D 
+@export var player: CharacterBody2D
 
 var tiles := {
-	"sea":[0, 0, [Vector2i(27, 5)]],
+	"sea": [0, 0, [Vector2i(27, 5)]],
 	"beach": [0, 0, [Vector2i(19, 9)]],
-	"Grasslands":[0, 0, [Vector2i(1, 9),Vector2i(0, 11), Vector2i(1, 11), Vector2i(2, 11), Vector2i(4, 11)]],
-	"forest": [0, 0, [Vector2i(7, 9),Vector2i(6, 11),Vector2i(7, 11), Vector2i(8, 11)]],
-	"desert": [0, 0, [Vector2i(19, 9),Vector2i(18, 11),Vector2i(19, 11), Vector2i(20, 11)]],
-	"snow": [0, 0, [Vector2i(19, 15),Vector2i(18, 17),Vector2i(19, 17), Vector2i(20, 17)]],
+	"Grasslands": [0, 0, [Vector2i(1, 9), Vector2i(0, 11), Vector2i(1, 11), Vector2i(2, 11), Vector2i(4, 11)]],
+	"forest": [0, 0, [Vector2i(7, 9), Vector2i(6, 11), Vector2i(7, 11), Vector2i(8, 11)]],
+	"desert": [0, 0, [Vector2i(19, 9), Vector2i(18, 11), Vector2i(19, 11), Vector2i(20, 11)]],
+	"snow": [0, 0, [Vector2i(19, 15), Vector2i(18, 17), Vector2i(19, 17), Vector2i(20, 17)]],
 }
-var biome_info := {#-2 or 2 is like negative or positive infinaty
+var biome_info := { # -2 or 2 is like negative or positive infinaty
 	"sea": [[-2, 0.04], [-2, 2], [-2, 2]],
 	"beach": [[0.04, 0.1], [-2, 2], [-2, 2]],
 	"Grasslands": [[0.1, 2], [-2, 2], [-2, 2]],
@@ -24,9 +24,9 @@ var biome_info := {#-2 or 2 is like negative or positive infinaty
 	"snow": [[0.4, 2], [-2, 2], [-2, 0.2]],
 }
 
-func Generating()-> void:
+func Generating() -> void:
 	world_seed = randi()
-	biome = BiomeGen.new(self.height*4, self.width*4, world_seed)
+	biome = BiomeGen.Biome2D.new(self.height * 2, self.width * 2, world_seed)
 	biome.set_biomes(biome_info, tile_map.local_to_map(player.position))
 	biome.render_tiles(tiles, tile_map, tile_map.local_to_map(player.position))
 	biome.height = self.height
@@ -46,4 +46,3 @@ func _process(delta):
 		biome.set_biomes(biome_info, player_tile_pos)
 		biome.render_tiles(tiles, tile_map, player_tile_pos)
 		biome.unload_distant_chunks(player_tile_pos, tile_map)
-
--- Scripts/biomeGen.gd
+++ Scripts/biomeGen.gd
@@ -1,18 +1,18 @@
 class_name BiomeGen
 
-var height : int
-var width : int
+var height: int
+var width: int
 var biomes := {}
 var alt_noise := FastNoiseLite.new()
 var ppt_noise := FastNoiseLite.new()
 var tem_noise := FastNoiseLite.new()
 
-func _init(height: int, width: int, world_seed: int):
-	self.height = height
-	self.width = width
+func _init(init_height: int, init_width: int, world_seed: int):
+	self.height = init_height
+	self.width = init_width
 	self.pre_noiseSetup(world_seed)
 
-func pre_noiseSetup(world_seed: int)-> void:
+func pre_noiseSetup(world_seed: int) -> void:
 	self.alt_noise.seed = world_seed
 	self.ppt_noise.seed = world_seed
 	self.tem_noise.seed = world_seed
@@ -28,52 +28,65 @@ func pre_noiseSetup(world_seed: int)-> void:
 	self.tem_noise.fractal_gain = 0.17
 	self.tem_noise.fractal_weighted_strength = -0.52
 
-func between(val, start, end)-> bool:
-	if start <= val and val < end:
-		return true
-	return false
+func getAltitude(pos: Vector2i) -> float:
+	return self.alt_noise.get_noise_2d(pos.x, pos.y)
 
-func set_biomes(biome_info: Dictionary, player_pos: Vector2i = Vector2i(0,0))-> void:
-	for x in self.width:
-		for y in self.height:
-			var pos := Vector2i(player_pos.x - (width/2) + x, player_pos.y - (height/2) + y)
-			var altitude: float = self.alt_noise.get_noise_2d(pos.x, pos.y)
-			var precipition: float = self.ppt_noise.get_noise_2d(pos.x, pos.y)
-			var temprature: float = self.tem_noise.get_noise_2d(pos.x, pos.y)
-			var biome
-			for key in biome_info.keys():
-				if (between(altitude, biome_info[key][0][0], biome_info[key][0][1]) and 
-					between(precipition, biome_info[key][1][0], biome_info[key][1][1]) and 
-					between(temprature, biome_info[key][2][0], biome_info[key][2][1])):
-						biome = key
-			if not self.biomes.has(pos):
-				self.biomes[pos] = [biome, Vector2i(-1,-1)]
+func getPrecipition(pos: Vector2i) -> float:
+	return self.ppt_noise.get_noise_2d(pos.x, pos.y)
 
-func render_tiles(tiles: Dictionary, tilemap: TileMap, player_pos: Vector2i)-> void:
-	for x in self.width:
-		for y in self.height:
-			var pos = Vector2i(player_pos.x - (width/2) + x, player_pos.y - (height/2) + y)
-			var biome: String = biomes[Vector2i(pos)][0]
-			if biomes[Vector2i(pos)][1] == Vector2i(-1,-1):
-				var temp = tiles[biome][2].pick_random()
-				tilemap.set_cell(tiles[biome][0], Vector2i(player_pos.x - (width/2) + x, player_pos.y - (height/2) + y), tiles[biome][1], temp)
-				self.biomes[Vector2i(pos)][1] = temp
-			else:
-				tilemap.set_cell(tiles[biome][0], Vector2i(player_pos.x - (width/2) + x, player_pos.y - (height/2) + y), tiles[biome][1], biomes[Vector2i(pos)][1])
+func getTemprature(pos: Vector2i) -> float:
+	return self.tem_noise.get_noise_2d(pos.x, pos.y)
+
+func getBiomes(pos: Vector2i) -> String:
+	return self.biomes[pos][0]
+
+class Biome2D extends BiomeGen:
+
+	func between(val, start, end) -> bool:
+		if start <= val and val < end:
+			return true
+		return false
+
+	func set_biomes(biome_info: Dictionary, player_pos: Vector2i = Vector2i(0, 0)) -> void:
+		for x in self.width:
+			for y in self.height:
+				var pos := Vector2i(player_pos.x - floor(width as float / 2) + x, player_pos.y - floor(height as float / 2) + y)
+				var altitude: float = getAltitude(pos)
+				var precipition: float = getPrecipition(pos)
+				var temprature: float = getTemprature(pos)
+				var biome
+				for key in biome_info.keys():
+					if (between(altitude, biome_info[key][0][0], biome_info[key][0][1]) and
+						between(precipition, biome_info[key][1][0], biome_info[key][1][1]) and
+						between(temprature, biome_info[key][2][0], biome_info[key][2][1])):
+							biome = key
+				if not self.biomes.has(pos):
+					self.biomes[pos] = [biome, Vector2i(-1, -1)]
+
+	func render_tiles(tiles: Dictionary, tilemap: TileMap, player_pos: Vector2i) -> void:
+		for x in width:
+			for y in height:
+				var pos := Vector2i(player_pos.x - floor(width as float / 2) + x, player_pos.y - floor(height as float / 2) + y)
+				var biome = biomes[pos][0]
+				var temp = biomes[pos][1]
+				if temp == Vector2i(-1, -1):
+					temp = tiles[biome][2].pick_random()
+					biomes[pos][1] = temp
+				tilemap.set_cell(tiles[biome][0], pos, tiles[biome][1], temp)
 				
-func unload_distant_chunks(player_pos, tilemap: TileMap)-> void:
-	var unload_distance_threshold := (width * 4) + 1
-	for chunk in biomes:
-		var distance_to_player = get_dist(chunk, player_pos)
-		if distance_to_player > unload_distance_threshold:
-			clear_chunk(chunk, tilemap)
-			biomes.erase(chunk)
+	func unload_distant_chunks(player_pos, tilemap: TileMap) -> void:
+		var unload_distance_threshold := (width * 4) + 1
+		for chunk in biomes:
+			var distance_to_player = get_dist(chunk, player_pos)
+			if distance_to_player > unload_distance_threshold:
+				clear_chunk(chunk, tilemap)
+				biomes.erase(chunk)
 
-func get_dist(p1, p2)-> float:
-	var resultant = p1 - p2
-	return sqrt(resultant.x ** 2 + resultant.y ** 2)
-	
-func clear_chunk(pos, tilemap: TileMap)-> void:
-	for x in range(width):
-		for y in range(height):
-			tilemap.set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), -1, Vector2(-1, -1), -1)
+	func get_dist(p1, p2) -> float:
+		var resultant = p1 - p2
+		return sqrt(resultant.x ** 2 + resultant.y ** 2)
+		
+	func clear_chunk(pos, tilemap: TileMap) -> void:
+		for x in range(width):
+			for y in range(height):
+				tilemap.set_cell(0, Vector2i(pos.x - floor(width as float / 2) + x, pos.y - floor(height as float / 2)), -1, Vector2(-1, -1), -1)
