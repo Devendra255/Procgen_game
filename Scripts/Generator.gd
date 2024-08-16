757d864e5c2349f7ecc2a5e04fdb51110d7fdd28
Devendra <devendramokal806@gmail.com>
2024-08-17 02:45:37 +0530

    Added river generation

--- Scripts/Generator.gd
+++ Scripts/Generator.gd
@@ -14,6 +14,7 @@ var tiles := {
 	"forest": [0, [Vector2i(7, 9), Vector2i(6, 11), Vector2i(7, 11), Vector2i(8, 11)]],
 	"desert": [0, [Vector2i(19, 9), Vector2i(18, 11), Vector2i(19, 11), Vector2i(20, 11)]],
 	"snow": [0, [Vector2i(19, 15), Vector2i(18, 17), Vector2i(19, 17), Vector2i(20, 17)]],
+	"river": [0, [Vector2i(28, 5)]],
 }
 var biome_info := { # -2 or 2 is like negative or positive infinaty
 	"sea": [[-2, 0.04], [-2, 2], [-2, 2]],
@@ -27,6 +28,7 @@ var biome_info := { # -2 or 2 is like negative or positive infinaty
 func Generating() -> void:
 	world_seed = randi()
 	biome = BiomeGen.Biome2D.new(self.height * 2, self.width * 2, world_seed)
+	biome.create_rivers(tile_map.local_to_map(player.position))
 	biome.set_biomes(biome_info, tile_map.local_to_map(player.position))
 	biome.render_tiles(tiles, tile_map, tile_map.local_to_map(player.position))
 	biome.height = self.height
@@ -35,14 +37,16 @@ func Generating() -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Generating()
-	
+	player_tile_pos = tile_map.local_to_map(player.position)
 func threading():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if player_tile_pos != tile_map.local_to_map(player.position):
-		player_tile_pos = tile_map.local_to_map(player.position)
+		biome.create_rivers(player_tile_pos)
 		biome.set_biomes(biome_info, player_tile_pos)
 		biome.render_tiles(tiles, tile_map, player_tile_pos)
 		biome.unload_distant_chunks(player_tile_pos, tile_map)
+
+	player_tile_pos = tile_map.local_to_map(player.position)
--- Scripts/biomeGen.gd
+++ Scripts/biomeGen.gd
@@ -3,9 +3,11 @@ class_name BiomeGen
 var height: int
 var width: int
 var biomes := {}
+var water := {}
 var alt_noise := FastNoiseLite.new()
 var ppt_noise := FastNoiseLite.new()
 var tem_noise := FastNoiseLite.new()
+var river_points := FastNoiseLite.new()
 
 func _init(init_height: int, init_width: int, world_seed: int):
 	self.height = init_height
@@ -16,7 +18,19 @@ func pre_noiseSetup(world_seed: int) -> void:
 	self.alt_noise.seed = world_seed
 	self.ppt_noise.seed = world_seed
 	self.tem_noise.seed = world_seed
+	self.river_points.seed = world_seed
+	self.river_points.noise_type = FastNoiseLite.TYPE_CELLULAR
+	self.river_points.frequency = 0.15
+	self.river_points.fractal_type = FastNoiseLite.FRACTAL_RIDGED
+	self.river_points.fractal_octaves = 5
+	self.river_points.fractal_lacunarity = 15.0
+	self.river_points.fractal_gain = -0.110
+	self.river_points.fractal_weighted_strength = 7.32
+	self.river_points.cellular_distance_function = FastNoiseLite.CellularDistanceFunction.DISTANCE_EUCLIDEAN_SQUARED
+	self.river_points.cellular_return_type = FastNoiseLite.CellularReturnType.RETURN_DISTANCE2_MUL
+	self.river_points.cellular_jitter = 0.9
 	self.alt_noise.frequency = 0.01
+	self.alt_noise.fractal_octaves = 5
 	self.ppt_noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
 	self.ppt_noise.frequency = 0.01
 	self.ppt_noise.fractal_octaves = 3
@@ -28,17 +42,20 @@ func pre_noiseSetup(world_seed: int) -> void:
 	self.tem_noise.fractal_gain = 0.17
 	self.tem_noise.fractal_weighted_strength = -0.52
 
-func getAltitude(pos: Vector2i) -> float:
+func get_altitude(pos: Vector2i) -> float:
 	return self.alt_noise.get_noise_2d(pos.x, pos.y)
 
-func getPrecipition(pos: Vector2i) -> float:
+func get_precipition(pos: Vector2i) -> float:
 	return self.ppt_noise.get_noise_2d(pos.x, pos.y)
 
-func getTemprature(pos: Vector2i) -> float:
+func get_temprature(pos: Vector2i) -> float:
 	return self.tem_noise.get_noise_2d(pos.x, pos.y)
 
-func getBiomes(pos: Vector2i) -> String:
+func get_biomes(pos: Vector2i) -> String:
 	return self.biomes[pos][0]
+
+func get_river_points_value(pos: Vector2i) -> float:
+	return self.river_points.get_noise_2d(pos.x, pos.y)
 
 class Biome2D extends BiomeGen:
 
@@ -51,21 +68,23 @@ class Biome2D extends BiomeGen:
 		for x in self.width:
 			for y in self.height:
 				var pos := Vector2i(player_pos.x - floor(width as float / 2) + x, player_pos.y - floor(height as float / 2) + y)
-				var altitude: float = getAltitude(pos)
-				var precipition: float = getPrecipition(pos)
-				var temprature: float = getTemprature(pos)
+				var altitude: float = get_altitude(pos)
+				var precipition: float = get_precipition(pos)
+				var temprature: float = get_temprature(pos)
 				var biome
-				for key in biome_info.keys():
-					if (between(altitude, biome_info[key][0][0], biome_info[key][0][1]) and
-						between(precipition, biome_info[key][1][0], biome_info[key][1][1]) and
-						between(temprature, biome_info[key][2][0], biome_info[key][2][1])):
-							biome = key
-				if not self.biomes.has(pos):
+				if not (self.biomes.has(pos) or self.water.has(pos)):
+					for key in biome_info.keys():
+						if (between(altitude, biome_info[key][0][0], biome_info[key][0][1]) and
+							between(precipition, biome_info[key][1][0], biome_info[key][1][1]) and
+							between(temprature, biome_info[key][2][0], biome_info[key][2][1])):
+								biome = key
 					self.biomes[pos] = [biome, Vector2i(-1, -1)]
+				if self.water.has(pos):
+					self.biomes[pos] = ["river", Vector2i(-1, -1)]
 
 	func render_tiles(tiles: Dictionary, tilemap: TileMapLayer, player_pos: Vector2i) -> void:
-		for x in width:
-			for y in height:
+		for x in self.width:
+			for y in self.height:
 				var pos := Vector2i(player_pos.x - floor(width as float / 2) + x, player_pos.y - floor(height as float / 2) + y)
 				var biome = biomes[pos][0]
 				var temp = biomes[pos][1]
@@ -75,7 +94,7 @@ class Biome2D extends BiomeGen:
 				tilemap.set_cell(pos, tiles[biome][0], temp)
 				
 	func unload_distant_chunks(player_pos, tilemap: TileMapLayer) -> void:
-		var unload_distance_threshold := (width * 4) + 1
+		var unload_distance_threshold := (width * 3) + 1
 		for chunk in biomes:
 			var distance_to_player = get_dist(chunk, player_pos)
 			if distance_to_player > unload_distance_threshold:
@@ -87,6 +106,37 @@ class Biome2D extends BiomeGen:
 		return sqrt(resultant.x ** 2 + resultant.y ** 2)
 		
 	func clear_chunk(pos, tilemap: TileMapLayer) -> void:
-		for x in range(width):
-			for y in range(height):
-				tilemap.set_cell(Vector2i(pos.x - floor(width as float / 2) + x, pos.y - floor(height as float / 2)), -1, Vector2(-1, -1))
+		for x in self.width:
+			for y in self.height:
+				tilemap.set_cell(Vector2i(pos.x - floor(width as float / 2) + x, pos.y - floor(height as float / 2) + y), -1, Vector2i(-1, -1), -1)
+
+	func create_rivers(player_pos: Vector2i = Vector2i(0, 0)) -> void:
+		for x in self.width:
+			for y in self.height:
+				var pos := Vector2i(player_pos.x - floor(width as float / 2) + x, player_pos.y - floor(height as float / 2) + y)
+				var water_point := [self.get_river_points_value(pos), self.get_altitude(pos)]
+				var itr_points := []
+				if water_point[0] >= -0.6 and water_point[1] > 0.3:
+					if not self.water.has(pos):
+						itr_points.append(pos)
+						while itr_points.size() > 0:
+							var itr_point = itr_points.pop_front()
+							self.water[itr_point] = "river"
+							var water_level = self.get_altitude(itr_point)
+							var neighbors = get_neighbors(itr_point)
+							for neighbor in neighbors:
+								if not self.water.has(neighbor):
+									var altitude = self.get_altitude(neighbor)
+									if altitude < water_level and altitude > 0:
+										self.water[neighbor] = "river"
+										if itr_points.size() < 3:
+											itr_points.append(neighbor)
+
+func get_neighbors(pos: Vector2i) -> Array:
+	var neighbors := []
+	for x in range(-1, 2):
+		for y in range(-1, 2):
+			if (x == 0 and y == 0):
+				continue
+			neighbors.append(Vector2i(pos.x + x, pos.y + y))
+	return neighbors
